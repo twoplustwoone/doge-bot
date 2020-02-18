c68d9e04b2a4ea169815ceb15423128e4586f74d
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2020-02-18 14:24:44 -0300

    add repsond

--- scripts/doge.js
+++ scripts/doge.js
@@ -2,6 +2,15 @@ const serviceFactory = require('../src/service');
 
 module.exports = function main(robot) {
     const service = serviceFactory(robot);
+    const commands = [
+        [/^:doge(.*):$/i, service.addDoge],
+        [/^(get doges|list doges|doge get|doge list)$/i, service.getDoges],
+        [/^(doge help)$/i, service.getHelp],
+        [/^(doge info)$/i, service.getInfo],
+        [/^(doge history)$/i, service.getHistory],
+    ]
+
+    commands.forEach(c => robot.respond(c[0], c[1]))
 
     robot.hear(/^:doge(.*):$/i, service.addDoge)
     robot.hear(/^(get doges|list doges|doge get|doge list)$/i, service.getDoges)
