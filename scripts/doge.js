3b48acc4c31a2f789d57688d19d58cefbcfb515f
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2020-02-18 14:55:29 -0300

    testing again

--- scripts/doge.js
+++ scripts/doge.js
@@ -2,23 +2,13 @@ const serviceFactory = require('../src/service');
 
 module.exports = function main(robot) {
     const service = serviceFactory(robot);
-    const commands = [
-        [/:doge(.*):/i, service.addDoge],
-        [/(get doges|list doges|doge get|doge list)/i, service.getDoges],
-        [/(doge help)/i, service.getHelp],
-        [/(doge info)/i, service.getInfo],
-        [/(doge history)/i, service.getHistory],
-    ]
 
-    commands.forEach(c => robot.hear(c[0], c[1]))
-
-    // robot.hear(/^:doge(.*):$/i, service.addDoge)
-    // robot.hear(/^(get doges|list doges|doge get|doge list)$/i, service.getDoges)
-    // robot.hear(/^(doge help)$/i, service.getHelp)
-    // robot.hear(/^(doge info)$/i, service.getInfo)
-    // robot.hear(/^(doge history)$/i, service.getHistory)
-
+    robot.hear(/:doge(.*):/i, service.addDoge)
+    robot.hear(/(get doges|list doges|doge get|doge list)/i, service.getDoges)
+    robot.hear(/(doge help)/i, service.getHelp)
+    robot.hear(/(doge info)/i, service.getInfo)
+    robot.hear(/(doge history)/i, service.getHistory)
     // TODO
     // robot.hear(/^(doge scan)$/i, service.scan)
-    robot.hear(/^(doge history)$/i, service.getHistory)
+    robot.hear(/(doge history)/i, service.getHistory)
 }
