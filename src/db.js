0a05b4fd33666f444d2173e74067c87c142e837b
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2020-02-19 14:03:26 -0300

    change db credential names

--- src/db.js
+++ src/db.js
@@ -1,10 +1,10 @@
 const db = require('knex')({
   client: 'pg',
   connection: {
-  host : process.env.PG_HOST,
-  user : process.env.PG_USER,
-  password : process.env.PG_PASSWORD,
-  database : process.env.PG_DATABASE,
+  host : process.env.DATABASE_HOST,
+  user : process.env.DATABASE_USER,
+  password : process.env.DATABASE_PASSWORD,
+  database : process.env.DATABASE_DATABASE,
   ssl: true
   }
 });
