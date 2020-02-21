100416e06ea32001e4d9cfd74e63d7a727a0e8da
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2020-02-21 11:22:38 -0300

    depend only on DATABASE_URL

--- src/db.js
+++ src/db.js
@@ -1,12 +1,24 @@
+const databaseUrl = process.env.DATABASE_URL
+
+const startOfUser = databaseUrl.indexOf('//') + 2
+const endOfUser = databaseUrl.indexOf(':', startOfUser)
+const user = databaseUrl.substring(startOfUser, endOfUser)
+
+const startOfPassword = endOfUser + 1
+const endOfPassword = databaseUrl.indexOf('@')
+const password = databaseUrl.substring(startOfPassword, endOfPassword)
+
+const startOfHost = endOfPassword + 1
+const endOfHost = databaseUrl.indexOf(':', startOfHost)
+const host = databaseUrl.substring(startOfHost, endOfHost)
+
+const startOfDatabase = databaseUrl.indexOf('/', endOfHost) + 1
+const endOfDatabase = databaseUrl.length - 1
+const database = databaseUrl.substring(startOfDatabase, endOfDatabase)
+
 const db = require('knex')({
   client: 'pg',
-  connection: {
-  host : process.env.DATABASE_HOST,
-  user : process.env.DATABASE_USER,
-  password : process.env.DATABASE_PASSWORD,
-  database : process.env.DATABASE_DATABASE,
-  ssl: true
-  }
+  connection: { host, user, password, database, ssl: true }
 });
 
 module.exports = db;
