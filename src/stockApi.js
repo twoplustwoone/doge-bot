5bfbfda9e36e8c61a91a371c0a9753175d80817c
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2022-07-18 10:01:18 -0400

    fix stockApi.js

--- src/stockApi.js
+++ src/stockApi.js
@@ -1,7 +1,11 @@
-const baseUri = 'https://api.twelvedata.com'
+const baseUri = "https://api.twelvedata.com";
 
-export const getStockPrice = (symbol) => {
-    return fetch(`${baseUri}/time_series/?symbol=${symbol}&interval=1min&outputsize=1&format=JSON%apikey=${process.env.TWELVE_DATA_API_KEY}`)
-        .then(response => response.json())
-        .then(data => data.values[0].open)
-}
+const getStockPrice = (symbol) => {
+  return fetch(
+    `${baseUri}/time_series/?symbol=${symbol}&interval=1min&outputsize=1&format=JSON%apikey=${process.env.TWELVE_DATA_API_KEY}`
+  )
+    .then((response) => response.json())
+    .then((data) => data.values[0].open);
+};
+
+module.exports = { getStockPrice };
