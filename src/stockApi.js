3593d51da32465e4581056f604b4ec6f0a0f3290
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2022-07-18 10:07:17 -0400

    dont log key

--- src/stockApi.js
+++ src/stockApi.js
@@ -2,11 +2,11 @@ const baseUri = "https://api.twelvedata.com";
 
 const getStockPrice = (symbol) => {
   return fetch(
-    `${baseUri}/time_series/?symbol=${symbol}&interval=1min&outputsize=1&format=JSON%apikey=${process.env.TWELVE_DATA_API_KEY}`
+    `${baseUri}/time_series/?symbol=${symbol}&interval=1min&outputsize=1&format=JSON&apikey=${process.env.TWELVE_DATA_API_KEY}`
   )
     .then((response) => response.json())
     .then((data) => {
-      console.log({ data, TWELVE_DATA_API_KEY: process.env.TWELVE_DATA_API_KEY });
+      console.log({ data });
       return data.values[0].open;
     });
 };
