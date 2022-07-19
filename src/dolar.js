cdc393d39918efee8613a1aa9aa763a84c8a04e7
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2022-07-19 16:33:50 -0400

    add dolar blue

---
+++ src/dolar.js
@@ -0,0 +1,17 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const url = "https://dolarhoy.com/";
+
+async function getDolarBlue() {
+  // Make an XHR request to get the latest dolar blue price from dolarhoy.com
+  const { data } = await axios.get(url);
+  const $ = cheerio.load(data);
+  const purchase = $(".tile.is-parent.is-5 .compra .val")
+    .text()
+    .replace("$", "");
+  const sale = $(".tile.is-parent.is-5 .venta .val").text().replace("$", "");
+  return (purchase + sale) / 2;
+}
+
+module.exports = { getDolarBlue };
