d846795b99df244ebb27286c322fc2d04e1330e5
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2022-07-19 16:45:32 -0400

    round number for doge blue

--- src/messages.js
+++ src/messages.js
@@ -1,39 +1,57 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs");
+const path = require("path");
 
 const getDogeMessage = ({ dogeCount, userId }) => {
-    return `Oh no! :doge: Looks like <@${userId}> just got doge'd! You're now at ${dogeCount} doge's. :doge:`
-}
+  return `Oh no! :doge: Looks like <@${userId}> just got doge'd! You're now at ${dogeCount} doge's. :doge:`;
+};
 
 const getDogeListMessage = ({ roomUsers, title }) => {
-    let message = ''
-    message += `:doge: *Doge'd ${title}* :doge:`
-    roomUsers.forEach(roomUser => message += `\n- ${roomUser.name}: ${roomUser.doge_count}`)
-    return message
-}
+  let message = "";
+  message += `:doge: *Doge'd ${title}* :doge:`;
+  roomUsers.forEach(
+    (roomUser) => (message += `\n- ${roomUser.name}: ${roomUser.doge_count}`)
+  );
+  return message;
+};
 
 const helpMessage = () => {
-    const helpText = fs.readFileSync(path.join(__dirname, 'assets', 'help.md'), 'utf-8')
-    return helpText
-}
+  const helpText = fs.readFileSync(
+    path.join(__dirname, "assets", "help.md"),
+    "utf-8"
+  );
+  return helpText;
+};
 
 const infoMessage = () => {
-    const infoText = fs.readFileSync(path.join(__dirname, 'assets', 'info.md'), 'utf-8')
-    return infoText
-}
+  const infoText = fs.readFileSync(
+    path.join(__dirname, "assets", "info.md"),
+    "utf-8"
+  );
+  return infoText;
+};
 
 const getRateMessage = () => {
-    return ':dogespin: Oops! Going too fast there :dogespin:'
-}
+  return ":dogespin: Oops! Going too fast there :dogespin:";
+};
 
 const getStockMessage = (price) => {
-    const stockText = `:dogespin: WOW! Stock price: ${price} :dogespin:`
-    return stockText
-}
+  const stockText = `:dogespin: WOW! Stock price: ${price} :dogespin:`;
+  return stockText;
+};
 
 const getDolarBlueMessage = (price) => {
-    const dolarBlueText = `:dogecoin: WOW! Dolar Blue price: ${price} :dogecoin:`
-    return dolarBlueText
-}
+  const dolarBlueText = `:dogecoin: WOW! Dolar Blue price: $${
+    Math.round(price * 100) / 100
+  } :dogecoin:`;
+  return dolarBlueText;
+};
 
-module.exports = { getDogeMessage, getDogeListMessage, helpMessage, infoMessage, getRateMessage, getStockMessage, getDolarBlueMessage }
+module.exports = {
+  getDogeMessage,
+  getDogeListMessage,
+  helpMessage,
+  infoMessage,
+  getRateMessage,
+  getStockMessage,
+  getDolarBlueMessage,
+};
