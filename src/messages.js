b1cfc38b855206a8108f55b2345182333a2ee520
Lele <45210281+llibarona@users.noreply.github.com>
2022-07-20 13:26:01 -0300

    add stock delta messages & stock/blue composition (#20)
    * add stock delta messages & stock/blue composition
    
    * fix code style
    
    Co-authored-by: Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>

--- src/messages.js
+++ src/messages.js
@@ -34,9 +34,17 @@ const getRateMessage = () => {
   return ":dogespin: Oops! Going too fast there :dogespin:";
 };
 
-const getStockMessage = (price) => {
-  const stockText = `:dogespin: WOW! Stock price: ${price} :dogespin:`;
-  return stockText;
+const getStockMessage = (price, delta = 0) => {
+  if (delta > 1) {
+    return `:dogespin: WOW! Stock price: ${price} :dogespin: much :stonks:`;
+  }
+  if (delta < -10) {
+    return `:dogespin: oh noo! Stock price: ${price} :dogespin: much :panik:`;
+  }
+  if (delta < -1) {
+    return `:dogespin: oh noo! Stock price: ${price} :dogespin: much :stonks_down:`;
+  }
+  return `:dogespin: WOW! Stock price: ${price} :dogespin:`;
 };
 
 const getDolarBlueMessage = (price) => {
@@ -44,6 +52,13 @@ const getDolarBlueMessage = (price) => {
     Math.round(price * 100) / 100
   } :dogecoin:`;
   return dolarBlueText;
+};
+
+const getCRMBlueMessage = (price) => {
+  const crmBlueText = `:dogecoin: WOW! CRM at Dolar Blue price: $${
+    Math.round(price * 100) / 100
+  } :dogecoin:`;
+  return crmBlueText;
 };
 
 module.exports = {
@@ -54,4 +69,5 @@ module.exports = {
   getRateMessage,
   getStockMessage,
   getDolarBlueMessage,
+  getCRMBlueMessage,
 };
--- src/stockApi.js
+++ src/stockApi.js
@@ -1,8 +1,8 @@
 const baseUri = "https://api.twelvedata.com";
 
-const getStockPrice = (symbol) => {
+const getStockPrice = (symbol, interval = "min") => {
   return fetch(
-    `${baseUri}/time_series/?symbol=${symbol}&interval=1min&outputsize=1&format=JSON&apikey=${process.env.TWELVE_DATA_API_KEY}`
+    `${baseUri}/time_series/?symbol=${symbol}&interval=1${interval}&outputsize=1&format=JSON&apikey=${process.env.TWELVE_DATA_API_KEY}`
   )
     .then((response) => response.json())
     .then((data) => {
